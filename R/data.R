f7e4e63a48ae92123492094549b011e80c0a5548
utubun <dmytro.strunin@gmail.com>
2023-02-20 15:27:14 +0100

    Completed #8 #7 #6

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' P. aeruginosa PAO1 CDS data
+#'
+#' CDS extracted from the full genome assembly of P. aeruginosa PAO1, assembly
+#' ASM676v1 provided by NCBI. Release date 2006-07-07
+#'
+#' @format ## 'PAO1CDS
+#' A DNAStringSet with 5572 dna sequences
+#' \describe{
+#'   \item{names}{Gene ID}
+#'   \item{width}{Length of the sequence}
+#'   \item{seq}{DNA sequence of the gene}
+#' }
+#' @source <https://www.ncbi.nlm.nih.gov/assembly/GCF_000006765.1/>
+"PAO1CDS"
+
+#' P. aeruginosa PA14 CDS data
+#'
+#' CDS extracted from the full genome assembly of P. aeruginosa PA14, assembly
+#' ASM1462v1 provided by NCBI. Release date 2013-10-06
+#'
+#' @format ## 'PAO1CDS
+#' A DNAStringSet with 5572 dna sequences
+#' \describe{
+#'   \item{names}{Gene ID}
+#'   \item{width}{Length of the sequence}
+#'   \item{seq}{DNA sequence of the gene}
+#' }
+#' @source <https://www.ncbi.nlm.nih.gov/assembly/GCF_000014625.1/>
+"PA14CDS"
